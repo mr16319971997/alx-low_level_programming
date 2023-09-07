5db9b7df6e7728d31954d8f5f12922ddc068666d
mr16319971997 <mr16319971631997@gmail.com>
2023-09-07 09:14:15 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-    puts("\"Programming is like building a multili    ngual puzzle");
-    return (0);
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
 }
