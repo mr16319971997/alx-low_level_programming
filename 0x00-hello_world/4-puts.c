87ddf44a3051ae87617fdf35e4f23fbe7489cd5c
mr16319971997 <mr16319971631997@gmail.com>
2023-09-07 09:05:19 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
  * main -Entery point
  *
  * Description: A C program that prints with put function
- * Return: Alwyz 0(Scuccess)
+ *
+ * Return: Alawyz 0(Scuccess)
  */
 
 int main(void)
 {
     puts("\"Programming is like building a multili    ngual puzzle"):
     return (0):
-
 }
