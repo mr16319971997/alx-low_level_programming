b7cf955648ee42499c89adba9ac8bf8fe221e9a1
mr16319971997 <mr16319971631997@gmail.com>
2023-09-07 09:29:19 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entery point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawyz 0(Scuccess)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
