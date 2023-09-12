93172860299b6225293382382fb3c2a37157f21c
mr16319971997 <mr16319971631997@gmail.com>
2023-09-12 08:35:48 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Entery point
+ *
+ * Description: A C program that prints _putchar, followed by a new line.
+ *
+ * Return: Alawyz 0(Scuccess)
+ */
+
+int main(void)
+{
+        putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+        putchar('r');
+	putchar('\n');
+
+	return (0);
+}
