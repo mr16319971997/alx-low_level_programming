07bf56985f8d06c042457c1b54476fd8c855985e
mr16319971997 <mr16319971631997@gmail.com>
2023-09-12 08:57:18 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
- * Return:0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
-void print_alphabet(void)
+int main(void)
 {
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
+	return (0);
 }
